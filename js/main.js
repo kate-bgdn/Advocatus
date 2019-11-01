5471c53ec6eecab92a38662bde13080e5b374455
kate-bgdn <katyakot291@gmail.com>
2019-11-01 15:27:38 +0200

    add header (js) scroll effect

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// header scroll effect
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
+//mobile navigation
+$("#navToggle").click(function() {
+    $(this).toggleClass("active");
+    $(".overlay").toggleClass("open");
+    // this line ▼ prevents content scroll-behind
+    $("body").toggleClass("locked");
+});
